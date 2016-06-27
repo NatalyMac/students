d2e3f91211799d403e3820345273a7121e4549cd
Natalia Kulychyk <travers.nk@gmail.com>
2016-06-28 00:47:35 +0300

    Update settings.py

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -85,11 +85,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
-        #'ENGINE': 'django.db.backends.mysql',
-        #'HOST': 'localhost',
-        #'USER': 'root',
-        #'PASSWORD': '',
-        #'NAME': 'students_db',
     }
 }
 
