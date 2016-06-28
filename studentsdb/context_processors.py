fa155b062439ddf0745d8170f30027dbb7b3ac0b
Nataly Kulychyk <kulychyk.nataliia@travers-group.com>
2016-06-28 11:16:13 +0300

    relocate

--- studentsdb/context_processors.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-#from .settings import PORTAL_URL
-
-def students_proc(request):
-    my_portal_url = 'http://' + request.get_host()
-    # из request получаем имя хоста, добавляем префикс http и возвращаем в шаблон
-    return {'PORTAL_URL': my_portal_url}
-    #return {'PORTAL_URL': PORTAL_URL}
